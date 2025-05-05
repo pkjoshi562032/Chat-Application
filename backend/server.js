378f065af86e8e6f98df55c7f88684ea1c400acb
Prabhat <pkjoshi562032@gmail.com>
2025-05-05 08:31:57 +0530

    Connected to DB Succesfully

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,17 @@
 import express from "express"
 import dotenv from "dotenv"
+import mongoose from "mongoose"
 
 dotenv.config()
 
+mongoose
+.connect(process.env.MONGO_URI)
+.then(() => {
+   console.log("Connected to MongoDB")
+})
+.catch((err) => {
+  console.log(err)
+})
 //To start server;
 const app = express()
 
